0cb3528337acf81f9fd70368a25f062b1b6522e7
Faruk <frkcn@bil.omu.edu.tr>
2013-03-14 16:49:13 +0200

    subject insert

---
+++ subject_insert.sql
@@ -0,0 +1,12 @@
+insert into subject (subject_no, stitle)
+values
+    (1, 'Veritabanı'),
+    (2, 'Bilgisayar Ağları'),
+    (3, 'Python'),
+    (4, 'Ruby'),
+    (5, 'Nesne'),
+    (6, 'Programlama'),
+    (7, 'Hesaplama Teknikleri'),
+    (8, 'Algoritma'),
+    (9, 'Özdevinirler'),
+    (10, 'Matematik');
